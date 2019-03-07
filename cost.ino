608354f6199bda9ee3334ae512cc321774fe5dbd
baselrabia <baselrabia@gmail.com>
2019-03-07 02:00:28 +0200

    update cost.ino
    edit some code 
    fix some little bugs

--- cost.ino
+++ cost.ino
@@ -1,16 +1,19 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
  // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7) 
+
 
 #include <Keypad.h>
 const byte ROWS = 4; 
 const byte COLS = 4; 
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2'},
-  {'3', '4'},
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
-byte rowPins[ROWS] = {7, 6}; 
-byte colPins[COLS] = {9, 8}; 
+byte rowPins[ROWS] = {6, 7,}; 
+byte colPins[COLS] = {8, 9,}; 
 Keypad newkeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 #include <Servo.h>
@@ -25,6 +28,8 @@ const int place[]={A0,A1,A2,A3};
 byte val[4];
 
 int millistime;
+ 
+
 
 
 
@@ -49,6 +54,21 @@ void setup() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void loop() { 
  
  for (int c=0; c<4; c++) {
@@ -58,9 +78,9 @@ void loop() {
 for (int x=0; x<4; x++) { 
 	if(val[x]==0){
 		closeServo(x);
-		 millistime = millis() ;
-		timecar[x] = millistime /5000 //time by min and min equal  5 s to be more faster 
-		cost[x] = timecar[x] / 2 //cost of min by egp as hour equal 30 egp
+		millistime = millis() ;
+		timecar[x] = millistime/5000 ; //time by min and min equal  5 s to be more faster 
+		cost[x] = timecar[x]/2 ; //cost of min by egp as hour equal 30 egp
 
 	       }
 }
@@ -72,13 +92,13 @@ lcd.print(timecar[0]); //prints time since program started
    delay(1000);
 lcd.print("min-"); 
 lcd.print(cost[0]);
-lcd.print("EGP//")
+lcd.print("EGP//");
 lcd.print("2-"); 
 lcd.print(timecar[1]); //prints time since program started
    delay(1000);
 lcd.print("min-"); 
 lcd.print(cost[1]);
-lcd.print("EGP")
+lcd.print("EGP");
 
 
 //second row print
@@ -89,13 +109,13 @@ lcd.print(timecar[2]); //prints time since program started
    delay(1000);
 lcd.print("min-"); 
 lcd.print(cost[2]);
-lcd.print("EGP//")
+lcd.print("EGP//");
 lcd.print("4-"); 
 lcd.print(timecar[3]); //prints time since program started
    delay(1000);
 lcd.print("min-"); 
 lcd.print(cost[3]);
-lcd.print("EGP")
+lcd.print("EGP");
 
   // scroll 13 positions (string length) to the left
   // to move it offscreen left:
@@ -124,37 +144,39 @@ delay(1000);
 // for keypad click to open one parking place .. and print number in serial com
 
   char key = newkeypad.getKey();
-
-if (Key == 1){
+  if (key) {
+   Serial.println(key);
+  }
+  
+  if (key == 1) {
   openServo(0);
   }
-if (Key == 2){
-  openServo(1);
+  else if (key == 2) {
+    openServo(1);
   }
-if (Key == 3){
-  openServo(2);
+  else if (key == 3) {
+   openServo(2);
   }
-if (Key == 4){
-  openServo(3);
+  else if (key == 4) {
+   openServo(3);
   }
-
-
+ 
 
 
 }
 
-void openServo(i)
+void openServo(int i)
 {
-    Serial.println(Key);
+
 	for (pos[i] = 0; pos[i]  <= 40; pos[i]  += 1) { servo[i].write(pos[i]);delay(30);} 
     delay(2000);   
     timecar[i] = 0; 
-	cost[i] = 0;    
+	  cost[i] = 0;    
 }
 
-void closeServo(x)
+void closeServo(int x)
 {
-	for (pos[x] = 40; pos[x] >= 0; pos[x] -= 1) { myservo[x].write(pos[x]);delay(30);}
+	for (pos[x] = 40; pos[x] >= 0; pos[x] -= 1) { servo[x].write(pos[x]);delay(30);}
     delay(1000);       
 }
 
